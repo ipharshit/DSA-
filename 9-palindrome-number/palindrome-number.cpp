189936893431d0a07c702cb011776d8390b0b672
ipharshit <harshitsharma.1835@gmail.com>
2026-03-31 01:50:05 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (64.95%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         int original=x;
-        long reversed=0;
+        long long reversed=0;
         if(x<0) return false;
         while(x!=0){
             int rem=x%10;
