b7ac3fd11fa0ad0a9aabb53494889a2690dd1b12
ipharshit <harshitsharma.1835@gmail.com>
2026-04-11 17:49:43 +0530

    Time: 37 ms (7.84%) | Memory: 19.4 MB (5.88%) - LeetSync

--- 2122-count-special-quadruplets/count-special-quadruplets.cpp
+++ 2122-count-special-quadruplets/count-special-quadruplets.cpp
@@ -2,11 +2,19 @@ class Solution {
 public:
     int countQuadruplets(vector<int>& nums) {
         int count=0;
-        for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                for(int k=j+1;k<nums.size();k++){
-                    for(int l=k+1;l<nums.size();l++){
-                        if(nums[i]+nums[j]+nums[k]==nums[l]) count++;
+        
+        for(int k=nums.size()-2;k>=2;k--){
+            unordered_map<int,int>mp;
+            for(int l=k+1;l<nums.size();l++){
+                int diff=nums[l]-nums[k];
+                mp[diff]++;
+            }
+            //check if the sum exists in map
+            for(int i=0;i<k;i++){
+                for(int j=i+1;j<k;j++){
+                    int sum=nums[i]+nums[j];
+                    if(mp.find(sum)!=mp.end()){
+                        count+=mp[sum];
                     }
                 }
             }
