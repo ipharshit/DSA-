6b75f389264b351b3a92089c43f532ade20db348
ipharshit <harshitsharma.1835@gmail.com>
2026-07-25 20:34:50 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (39.08%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -9,17 +9,28 @@ public:
     //     return {};
     // }
      
-    vector<int> twoSum(vector<int>& nums, int target) {
+    // vector<int> twoSum(vector<int>& nums, int target) {
+    //     unordered_map<int,int>mp;
+    //     for(int i=0;i<nums.size();i++){
+    //         mp[nums[i]]=i;
+    //     }
+    //     for(int i=0;i<nums.size();i++){
+    //         int t=(target-nums[i]);
+    //         // if(i==mp[t]) continue;
+    //         if(mp.find(t)!=mp.end()&&i!=mp[t]) return {i,mp[t]};
+    //     }
+    //     return {};
+    // }
+
+     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int,int>mp;
         for(int i=0;i<nums.size();i++){
+            int comp=target-nums[i];
+            if(mp.find(comp)!=mp.end()) return {i,mp[comp]};
             mp[nums[i]]=i;
-            // if(mp.find(nums[i])!=mp.end()) return {mp[i],i};
+
         }
-        for(int i=0;i<nums.size();i++){
-            int t=(target-nums[i]);
-            // if(i==mp[t]) continue;
-            if(mp.find(t)!=mp.end()&&i!=mp[t]) return {i,mp[t]};
-        }
+        
         return {};
     }
 };
