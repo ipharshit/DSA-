e2d4affe70f8ec5b93500fc8c85a4d591ceb3d4c
ipharshit <harshitsharma.1835@gmail.com>
2026-07-27 22:55:40 +0530

    Time: 0 ms (100.00%) | Memory: 30.3 MB (39.22%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,35 +1,20 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        int i=0;int j=0;
-        vector<int>ans;
-        while(j<nums.size() and nums[j]<0){
-            
-            j++;
-        }
-        i=j-1;
-        
-        while(i>=0 and j<nums.size()){
-            if(abs(nums[i])>=nums[j]){
-                int sq=nums[j]*nums[j];
-                ans.push_back(sq);
-                j++;
+        int i=0;
+        int j=nums.size()-1;
+        vector<int>ans(nums.size());
+        int k=ans.size()-1;
+        while(i<=j){
+            if(abs(nums[i])>abs(nums[j])){
+                ans[k]=nums[i]*nums[i];
+                i++;
+            } 
+            else {
+                ans[k]=nums[j]*nums[j];
+                j--;
             }
-            else{
-                int sq=nums[i]*nums[i];
-                ans.push_back(sq);
-                i--;
-            }
-        }
-        //yaha tak aa gaya
-        //matlab koi 1 array khali ho gya
-        while(i>=0){
-            ans.push_back(nums[i]*nums[i]);
-            i--;
-        }
-        while(j<nums.size()){
-            ans.push_back(nums[j]*nums[j]);
-            j++;
+            k--;
         }
         return ans;
     }
