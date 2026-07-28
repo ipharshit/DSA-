061bcf8bd2d96d8a14808814e273a433877f7bd1
ipharshit <harshitsharma.1835@gmail.com>
2026-07-28 09:44:54 +0530

    Time: 117 ms (22.42%) | Memory: 83.2 MB (5.05%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
@@ -3,11 +3,6 @@ public:
     string smallestPalindrome(string s) {
         string ans="";
         int n=s.size();
-        unordered_map<char,int>mp;
-        for(int i=0;i<n;i++){
-            char ch=s[i];
-            mp[ch]++;
-        }
         string left="",right="";
         string middle="";
         for(int i=0;i<n/2;i++){
