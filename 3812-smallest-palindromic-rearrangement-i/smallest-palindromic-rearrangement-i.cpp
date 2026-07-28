84f2b0571f40eb011378ffac5e4b939182f9ddc7
ipharshit <harshitsharma.1835@gmail.com>
2026-07-28 11:58:25 +0530

    Time: 49 ms (49.89%) | Memory: 76.5 MB (27.25%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
@@ -26,29 +26,20 @@ public:
     string smallestPalindrome(string s) {
         string ans="";
         string left="",middle="";
-        unordered_map<char,int>mp;
+        vector<int>mp(26,0);
         for(int i=0;i<s.size();i++){
-            mp[s[i]]++;
+            mp[s[i]-'a']++;
         }
         for(char ch='a';ch<='z';ch++){
-            if(mp.find(ch)!=mp.end()){
-                if(mp[ch]&1){
-                    // the freq is odd means it has left + 1 middle
-                    int copy=mp[ch]/2;
-                    while(copy--){
-                        left+=ch;
-                    }
+                if(mp[ch-'a']&1){
+                    // 1 middle
                     //yaha tak left completed
                     middle+=ch;
                 }
-                else{
-                    // even count means no middle
-                    int copy=mp[ch]/2;
-                     while(copy--){
+                int copy=mp[ch-'a']/2;
+                    while(copy--){
                         left+=ch;
                     }
-                }
-            }
         }
         ans+=left+middle;
         reverse(left.begin(),left.end());
