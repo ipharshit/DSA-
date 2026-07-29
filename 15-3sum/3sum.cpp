64f3b1f3b98f3ba9a2afb07c6dabe4a4860c8bd4
ipharshit <harshitsharma.1835@gmail.com>
2026-07-29 21:21:17 +0530

    Time: 45 ms (71.37%) | Memory: 29 MB (73.61%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,26 +1,46 @@
-#include<vector>
+
+// class Solution {
+// public:
+//     vector<vector<int>> threeSum(vector<int>& nums) {
+//         vector<vector<int>>v;
+//         sort(nums.begin(),nums.end());
+//         for(int i=0;i<nums.size();i++){
+//             if(i>0 and nums[i-1]==nums[i]) continue;
+//             for(int j=i+1;j<nums.size();j++){
+//                 if(j>i+1 and nums[j-1]==nums[j]) continue;
+//                 for(int k=j+1;k<nums.size();k++){
+//                     if(k>j+1 and nums[k-1]==nums[k]) continue;
+//                     if(nums[i] + nums[j] + nums[k] == 0) v.push_back({nums[i], nums[j], nums[k]});
+//                 }
+//             }
+//         }
+//         return v;
+//     }
+// };
+
+
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>>v;
         sort(nums.begin(),nums.end());
-        vector<vector<int>>ans;
-
-        for(int k=0;k<nums.size();k++){
-             if(k>0 and nums[k]==nums[k-1]) continue;
-            int i=k+1;
-            int j=nums.size()-1;
-            while(i<j){
-                int sum=nums[i]+nums[j];
-                if(sum==-nums[k]){ ans.push_back({nums[k],nums[i],nums[j]});
-                i++;
-                j--;
-                       while(i<j and nums[i]==nums[i-1]) i++;
-       while(j>i and nums[j]==nums[j+1]) j--;
+        for(int i=0;i<nums.size();i++){
+            int j=i+1;
+            int k=nums.size()-1;
+            if(i>0 and nums[i]==nums[i-1]) continue;
+            int target=0-nums[i];
+            while(j<k){
+                if(nums[j]+nums[k]==target){
+                    v.push_back({nums[i],nums[j],nums[k]});
+                    j++;
+                    k--;
+                    while(j<k and nums[j]==nums[j-1]) j++;
+                    while(j<k and nums[k]==nums[k+1]) k--;
+                }
+                else if(nums[j]+nums[k]<target) j++;
+                else k--;
             }
-            else if(sum>-nums[k]) j--;
-            else i++;
         }
-    }
-    return ans;
+        return v;
     }
 };
