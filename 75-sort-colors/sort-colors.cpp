ef18cd5fe46073873295ed9a61dfed686eaa9a7e
ipharshit <harshitsharma.1835@gmail.com>
2026-07-30 21:15:38 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (79.65%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int low=0,mid=low,high=nums.size()-1;
-        while(mid<=high){
-            if(nums[mid]==0){
-                //isko topper mein bhejo
-                swap(nums[mid],nums[low]);
-                mid++;
-                low++;
+        // sort(nums.begin(),nums.end());
+        int i=0;
+        int j=0;
+        int k=nums.size()-1;
+        while(j<=k){
+            if(nums[j]==2){
+                swap(nums[j],nums[k--]);
             }
-            else if(nums[mid]==1){
-                //nice
-                mid++;
+            else if(nums[j]==0){
+                swap(nums[j++],nums[i++]);
             }
-            else{
-                //2 ko backers mein bhejo
-                swap(nums[mid],nums[high]);
-                high--;
-            }
+            else j++;
         }
     }
 };
