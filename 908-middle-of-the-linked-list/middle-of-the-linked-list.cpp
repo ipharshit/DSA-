58755400384c098dd09888777341701f7f020854
ipharshit <harshitsharma.1835@gmail.com>
2026-08-05 22:05:48 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (27.21%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -8,21 +8,36 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
-public:
-int solve(ListNode* head){
-    auto temp=head;
-    int l=0;
-    while(temp) {
-        l++;
-        temp=temp->next;
-    }
-    return l;
-}
+// class Solution {
+// public:
+// int solve(ListNode* head){
+//     auto temp=head;
+//     int l=0;
+//     while(temp) {
+//         l++;
+//         temp=temp->next;
+//     }
+//     return l;
+// }
+//     ListNode* middleNode(ListNode* head) {
+//         int length=solve(head);
+//         auto temp=head;
+//         for(int i=1;i<=length/2;i++) temp=temp->next;
+//         return temp;
+//     }
+// };
+
+// method 2 -> slow and fast pointers
+
+class Solution{
+    public:
     ListNode* middleNode(ListNode* head) {
-        int length=solve(head);
-        auto temp=head;
-        for(int i=1;i<=length/2;i++) temp=temp->next;
-        return temp;
+        auto slow=head;
+        auto fast=head;
+        while(fast and fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
     }
 };
