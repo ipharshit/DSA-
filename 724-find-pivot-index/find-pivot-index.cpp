83f2f82c762708370d0de8bbcb356c9f8730ded3
ipharshit <harshitsharma.1835@gmail.com>
2026-08-08 22:17:01 +0530

    Time: 0 ms (100.00%) | Memory: 35.8 MB (56.46%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -1,19 +1,38 @@
+// class Solution {
+// public:
+//     int pivotIndex(vector<int>& nums) {
+//         int n=nums.size();
+//         vector<int>prefix(n);
+//         vector<int>suffix(n);
+//         prefix[0]=0;
+//         suffix[n-1]=0;
+//         for(int i=1;i<nums.size();i++){
+//             prefix[i]=prefix[i-1]+nums[i-1];
+//         }
+//         for(int i=n-2;i>=0;i--){
+//             suffix[i]=suffix[i+1]+nums[i+1];
+//         }
+//         for(int i=0;i<nums.size();i++){
+//             if(prefix[i]==suffix[i]) return i;
+//         }
+//         return -1;
+//     }
+// };
+
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
         int n=nums.size();
-        vector<int>prefix(n);
-        vector<int>suffix(n);
-        prefix[0]=0;
-        suffix[n-1]=0;
-        for(int i=1;i<nums.size();i++){
-            prefix[i]=prefix[i-1]+nums[i-1];
-        }
-        for(int i=n-2;i>=0;i--){
-            suffix[i]=suffix[i+1]+nums[i+1];
-        }
+        int left=0;
+        int total=0;
         for(int i=0;i<nums.size();i++){
-            if(prefix[i]==suffix[i]) return i;
+            total+=nums[i];
+        }
+        if(total-nums[0]==0) return 0;
+        for(int i=1;i<nums.size();i++){
+            left+=nums[i-1];
+            int right=total-left-nums[i];
+            if(left==right) return i;
         }
         return -1;
     }
