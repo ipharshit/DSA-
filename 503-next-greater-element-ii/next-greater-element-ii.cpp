18e66bf90b97151807098130dc97ae7871ba5e92
ipharshit <harshitsharma.1835@gmail.com>
2026-08-15 21:59:49 +0530

    Time: 4 ms (59.08%) | Memory: 28.5 MB (40.09%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+       vector<int>ans(nums.size());
+       stack<int>st;
+       for(int i=2*nums.size()-1;i>=0;i--){
+        while(!st.empty() and st.top()<=nums[i%nums.size()]) st.pop();
+        if(i<nums.size()){
+        if(st.empty()) ans[i]=-1;
+        else ans[i]=st.top();
+        }
+        // current element to push krna hi h
+        st.push(nums[i%nums.size()]);
+       } 
+       return ans;
+    }
+};
