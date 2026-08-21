37a93a313b4aae9bfa0f1e98f79179e46fab80a0
ipharshit <harshitsharma.1835@gmail.com>
2026-08-21 22:39:32 +0530

    Time: 26 ms (80.11%) | Memory: 72.7 MB (19.58%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,8 +1,26 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        // sorting method
-        sort(nums.rbegin(),nums.rend());
-        return nums[k-1];
+        // heap method
+        priority_queue<int,vector<int>,greater<int>>pq; // min heap
+        // arrrange 1st k elements in the min-heap
+        for(int i=0;i<k;i++){
+            pq.push(nums[i]);
+        }
+        // now compare remaining with the top(smallest)
+        for(int i=k;i<nums.size();i++){
+            if(nums[i]<pq.top()){
+                // yeh to kabhi answer ban hi nahi  sakt abecause
+                // yeh mere top se chota h -> aur niche to sab top se bhi bde h
+                continue;
+            }
+            else{
+                // yeh mere top se bada h
+                // it can be my ans
+                pq.pop();
+                pq.push(nums[i]);
+            }
+        }
+    return pq.top();
     }
 };
