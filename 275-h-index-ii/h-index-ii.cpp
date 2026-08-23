459c8a7123eab2d58ccb057730635a91def019b6
ipharshit <harshitsharma.1835@gmail.com>
2026-08-23 19:26:37 +0530

    Time: 0 ms (100.00%) | Memory: 22.6 MB (47.66%) - LeetSync

--- 275-h-index-ii/h-index-ii.cpp
+++ 275-h-index-ii/h-index-ii.cpp
@@ -1,34 +1,22 @@
 class Solution {
 public:
-bool canPublishHPapers(vector<int>&nums,int mid){
-    int countPapers=0;
-    for(int i=0;i<nums.size();i++){
-        if(nums[i]>=mid){
-            // i can publish this research papers-> i have enough citations
-            countPapers++;
-        }
-    }
-    return countPapers>=mid;
-}
     int hIndex(vector<int>& citations) {
         // define saerch space
         // i can have 0 papers that shoulkd have h citations
         int s=0;
         // i can atmost have n papers that can have atleast h citatations
-        int e=citations.size();
-        int ans=-1;
+        int e=citations.size()-1;
+        int n=citations.size();
+        int ans=0;
         while(s<=e){
             int mid=s+((e-s)>>1);
-            if(canPublishHPapers(citations,mid)){
-                // i have enough papers that have atleast h citations
-                ans=mid;
-                // atleast yani-> apne phele valo se jyada
-                s=mid+1;
+            if(citations[mid]>=n-mid){
+                // i have enough papers from mid to end
+                ans=n-mid;
+                e=mid-1;
             }
             else{
-                // i do not have enough papers that have atleast h citations
-                // the bar hi high
-                e=mid-1;
+                s=mid+1;
             }
         }
         return ans;
